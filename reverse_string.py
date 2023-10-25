f7f5b5fc0c4f36543cc9f9866808587008f1f69c
20Abdelrahman02 <120818398+20Abdelrahman02@users.noreply.github.com>
2023-10-25 19:21:54 +0300

    Update reverse_string.py

--- reverse_string.py
+++ reverse_string.py
@@ -1,1 +1,8 @@
+def reverse_string(input_string):
+  
+    return input_string[::-1]
 
+# Example usage:
+input_str = "Hello, World!"
+reversed_str = reverse_string(input_str)
+print("Reversed String:", reversed_str)  # Output: Reversed String: !dlroW ,olleH
